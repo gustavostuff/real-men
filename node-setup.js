2a329279ce1b59be03d557c2027a03e0cb37c09f
Gus <ing.glaragomez@gmail.com>
2018-10-25 11:42:48 -0700

    stick more to npm standards

--- node-setup.js
+++ node-setup.js
@@ -1,107 +1,108 @@
-const exec = require('child_process').exec;
-const utils = require('./utils');
+const exec = require('child_process').exec
+const utils = require('./utils')
+const to = require('./to')
 
-manageErrors = (errors) => {
+let manageErrors = (errors) => {
   return new Promise((resolve, reject) => {
     errors.forEach((err) => {
-      if(err) {
-        reject(err);
+      if (err) {
+        reject(err)
       }
-    });
+    })
 
-    resolve(utils.OK_CODE);
-  });
-};
+    resolve(utils.OK_CODE)
+  })
+}
 
-installHBS = () => {
+let installHBS = () => {
   return new Promise((resolve, reject) => {
-    console.log('    > Installing hbs...');
+    console.log('    > Installing hbs...')
 
-    exec(`npm i hbs --save`, async(err, stdout, stderr) => {
+    exec(`npm i hbs --save`, async (err, stdout, stderr) => {
       let error1, ok;
-      [error1, ok] = await to(manageErrors([err]));
-      
-      if(!ok) { 
-        reject(error1);
+      [error1, ok] = await to(manageErrors([err]))
+
+      if (!ok || error1) {
+        reject(error1)
       } else {
-        resolve(utils.OK_CODE);
+        resolve(utils.OK_CODE)
       }
-    });
-  });
-};
+    })
+  })
+}
 
-installMongoose = () => {
+let installMongoose = () => {
   return new Promise((resolve, reject) => {
-    console.log('    > Installing mongoose...');
+    console.log('    > Installing mongoose...')
 
-    exec(`npm i mongoose --save`, async(err, stdout, stderr) => {
+    exec(`npm i mongoose --save`, async (err) => {
       let error1, error2, ok;
       [error1, ok] = await to(manageErrors([err]));
-      [error2, ok] = await to(installHBS());
-      
-      if(!ok) { 
-        reject([error1, error2]);
+      [error2, ok] = await to(installHBS())
+
+      if (!ok || error1 || error2) {
+        reject(new Error('Error while installing mongoose'))
       } else {
-        resolve(utils.OK_CODE);
+        resolve(utils.OK_CODE)
       }
-    });
-  });
-};
+    })
+  })
+}
 
-installBodyParser = () => {
+let installBodyParser = () => {
   return new Promise((resolve, reject) => {
-    console.log('    > Installing body-parser...');
+    console.log('    > Installing body-parser...')
 
-    exec(`npm i body-parser --save`, async(err, stdout, stderr) => {
+    exec(`npm i body-parser --save`, async (err) => {
       let error1, error2, ok;
       [error1, ok] = await to(manageErrors([err]));
-      [error2, ok] = await to(installMongoose());
-      
-      if(!ok) { 
-        reject([error1, error2]);
+      [error2, ok] = await to(installMongoose())
+
+      if (!ok || error1 || error2) {
+        reject(new Error('Error while installing body-parser'))
       } else {
-        resolve(utils.OK_CODE);
+        resolve(utils.OK_CODE)
       }
-    });
-  });
-};
+    })
+  })
+}
 
-installExpress = () => {
+let installExpress = () => {
   return new Promise((resolve, reject) => {
-    console.log('    > Installing express...');
+    console.log('    > Installing express...')
 
-    exec(`npm i express --save`, async(err, stdout, stderr) => {
+    exec(`npm i express --save`, async (err) => {
       let error1, error2, ok;
       [error1, ok] = await to(manageErrors([err]));
-      [error2, ok] = await to(installBodyParser());
-      
-      if(!ok) { 
-        reject([error1, error2]);
+      [error2, ok] = await to(installBodyParser())
+
+      if (!ok || error1 || error2) {
+        reject(new Error('Error while installing express'))
       } else {
-        resolve(utils.OK_CODE);
+        resolve(utils.OK_CODE)
       }
-    });
-  });
-};
+    })
+  })
+}
 
-init = (folderName) => {
+let init = (folderName) => {
   return new Promise((resolve, reject) => {
-    console.log(`--> Intalling node dependencies in ${folderName}/`);
+    console.log(`--> Intalling node dependencies in ${folderName}/`)
 
-    exec(`npm init -y`, async(err, stdout, stderr) => {
+    exec(`npm init -y`, async (err) => {
       let error1, error2, ok;
       [error1, ok] = await to(manageErrors([err]));
-      [error2, ok] = await to(installExpress());
-      
-      if(!ok) { 
-        reject([error1, error2]);
+      [error2, ok] = await to(installExpress())
+
+      if (!ok || error1 || error2) {
+        reject(new Error('Error while initializing project'))
       } else {
-        resolve(utils.OK_CODE);
+        resolve(utils.OK_CODE)
       }
-    });
-  });
-};
+    })
+  })
+}
 
 module.exports = {
   init
-};
+}
